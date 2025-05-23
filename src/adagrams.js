c4a9b39011f405735d35b9a1640081115c9300d3
eugeniakashanskaya <jkashanskaya@gmail.com>
2025-05-23 14:01:57 -0700

    W3: completes scoreWord, test, all tests pass

--- src/adagrams.js
+++ src/adagrams.js
@@ -61,7 +61,51 @@ export const usesAvailableLetters = (input, lettersInHand) => {
 };
 
 export const scoreWord = (word) => {
-  // Implement this method for wave 3
+  if (!word) {
+    return 0;
+  };
+  
+  const letterPoints = {
+    'A': 1, 
+    'B': 3, 
+    'C': 3, 
+    'D': 2, 
+    'E': 1,
+    'F': 4, 
+    'G': 2, 
+    'H': 4, 
+    'I': 1, 
+    'J': 8,
+    'K': 5, 
+    'L': 1, 
+    'M': 3, 
+    'N': 1, 
+    'O': 1,
+    'P': 3, 
+    'Q': 10, 
+    'R': 1, 
+    'S': 1, 
+    'T': 1,
+    'U': 1, 
+    'V': 4, 
+    'W': 4, 
+    'X': 8, 
+    'Y': 4, 
+    'Z': 10,
+    };
+  
+  let wordScore = 0;
+  const upperWord = word.toUpperCase();
+
+  for (let letter of upperWord) {
+    wordScore += letterPoints[letter] || 0;  
+  }
+
+  if (upperWord.length >= 7 && upperWord.length <= 10) {
+    wordScore += 8;
+  }
+  
+  return wordScore;
 };
 
 export const highestScoreFrom = (words) => {
